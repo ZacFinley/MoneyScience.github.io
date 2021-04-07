797b2b57955ec6f4f3a591b64a74e57afa48729c
Zachery Finley <jenkinsmrz14@yahoo.com>
2021-04-06 21:39:24 -0600

    article editing and addition. Disclaimer creation

--- README.md
+++ README.md
@@ -8,10 +8,6 @@ TODO:
 
 -new name for site (look for domain)
 
-Add a disclaimer that this is for educational purposes and it can only be as accurate as the information put in.
-
-- finish started articles
-    - add ability to have a date for future publishing of articles (write it and add it to site now but it wont show until the date listed)
-
 Later
 stock price percentage to dividend amount
+Add a disclaimer modal
--- js/header.js
+++ js/header.js
@@ -36,5 +36,6 @@ function siteNavigation() {
 }
 
 function siteFooter() {
+    document.getElementById("footer").innerHTML += "<div class='siteFooter'>This site is for educational purposes only. There has not and never will be any investing advise. All decisions about a persons money shall be discussed with a financial advisor and/or tax professional and made solely by the individual. To make the best decision for an individuals' situation they must practice due diligence to understand the financial information that may impact them.<br><br>There is no way to create an account and therefore no way to save user information. Feel free to print any calculations as all input will be lost from the previous screen when moving to another page. As an extra layer of protection please do not enter account numbers.</div>";
     document.getElementById("footer").innerHTML += "<div class='siteFooter'>Created and Maintained by Zac McElfresh<div align=center class='counter'><a href='https://www.counter12.com'><img src='https://www.counter12.com/img-zc210463c1wAWcd9-50.gif' border='0' alt='free web counter'></a><script type='text/javascript' src='https://www.counter12.com/ad.js?id=zc210463c1wAWcd9'></script></div></div>";
 }
