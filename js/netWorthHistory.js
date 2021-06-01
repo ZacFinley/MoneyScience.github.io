3447dc20e58a5a356f08c5a069904b8113d33820
Zachery Finley <jenkinsmrz14@yahoo.com>
2021-06-01 01:19:36 -0600

    new features to net worth history page

--- js/netWorthHistory.js
+++ js/netWorthHistory.js
@@ -44,11 +44,6 @@ function calculateAccountBalances() {
     }
 }
 
-//function getTotalChartArray() {
-//    calculateAccountBalances();
-//    return masterAccountBalances;
-//}
-
 function addAccount(){
     masterAccountBalances[0].push('New Account');
     for (var i = 1; i < masterAccountBalances.length; i++) {
@@ -75,17 +70,65 @@ function reprintTable(){
     // Net Worth Row
     document.getElementById("inputTable").innerHTML += updateNetWorthRow();
     
-    // Net Worth Growth Row
-    document.getElementById("inputTable").innerHTML += updateNetWorthGrowthRow();
+    // Net Worth Growth Row Dollar
+    document.getElementById("inputTable").innerHTML += updateNetWorthGrowthRowDollar();
+    
+    // Net Worth Growth Row Percent
+    document.getElementById("inputTable").innerHTML += updateNetWorthGrowthRowPercent();
+    updateMasterNetWorthSection();
     drawChart();
 }
 
 function updateNetWorthRow() {
-    return "<tr><td>Net Worth</td><td id='startNetWorth'>$" + parseFloat(masterAccountBalances[1][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='janNetWorth'>$" + parseFloat(masterAccountBalances[2][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='febNetWorth'>$" + parseFloat(masterAccountBalances[3][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='marNetWorth'>$" + parseFloat(masterAccountBalances[4][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='aprNetWorth'>$" + parseFloat(masterAccountBalances[5][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='mayNetWorth'>$" + parseFloat(masterAccountBalances[6][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='junNetWorth'>$" + parseFloat(masterAccountBalances[7][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='julNetWorth'>$" + parseFloat(masterAccountBalances[8][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='augNetWorth'>$" + parseFloat(masterAccountBalances[9][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='sepNetWorth'>$" + parseFloat(masterAccountBalances[10][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='octNetWorth'>$" + parseFloat(masterAccountBalances[11][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='novNetWorth'>$" + parseFloat(masterAccountBalances[12][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='decNetWorth'>$" + parseFloat(masterAccountBalances[13][1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td></tr>";
+    var monthlyNetWorths = [];
+    for (var i = 1; i < 14; i++) {
+        if (!isNaN(masterAccountBalances[i][1]) && isFinite(masterAccountBalances[i][1])) {
+            monthlyNetWorths.push(masterAccountBalances[i][1]);
+        }
+        else {
+            monthlyNetWorths.push(0);
+        }
+    }
+    return "<tr><td>Net Worth</td><td id='startNetWorth'>$" + parseFloat(monthlyNetWorths[0]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='janNetWorth'>$" + parseFloat(monthlyNetWorths[1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='febNetWorth'>$" + parseFloat(monthlyNetWorths[2]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='marNetWorth'>$" + parseFloat(monthlyNetWorths[3]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='aprNetWorth'>$" + parseFloat(monthlyNetWorths[4]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='mayNetWorth'>$" + parseFloat(monthlyNetWorths[5]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='junNetWorth'>$" + parseFloat(monthlyNetWorths[6]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='julNetWorth'>$" + parseFloat(monthlyNetWorths[7]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='augNetWorth'>$" + parseFloat(monthlyNetWorths[8]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='sepNetWorth'>$" + parseFloat(monthlyNetWorths[9]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='octNetWorth'>$" + parseFloat(monthlyNetWorths[10]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='novNetWorth'>$" + parseFloat(monthlyNetWorths[11]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='decNetWorth'>$" + parseFloat(monthlyNetWorths[12]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td></tr>";
 }
 
-function updateNetWorthGrowthRow() {
-    return "<tr><td>Growth</td><td id='startNetWorthGrowth'></td><td id='janNetWorthGrowth'>" + parseFloat((masterAccountBalances[2][1]-masterAccountBalances[1][1])/masterAccountBalances[1][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='febNetWorthGrowth'>" + parseFloat((masterAccountBalances[3][1]-masterAccountBalances[2][1])/masterAccountBalances[2][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='marNetWorthGrowth'>" + parseFloat((masterAccountBalances[4][1]-masterAccountBalances[3][1])/masterAccountBalances[3][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='aprNetWorthGrowth'>" + parseFloat((masterAccountBalances[5][1]-masterAccountBalances[4][1])/masterAccountBalances[4][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='mayNetWorthGrowth'>" + parseFloat((masterAccountBalances[6][1]-masterAccountBalances[5][1])/masterAccountBalances[5][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='junNetWorthGrowth'>" + parseFloat((masterAccountBalances[7][1]-masterAccountBalances[6][1])/masterAccountBalances[6][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='julNetWorthGrowth'>" + parseFloat((masterAccountBalances[8][1]-masterAccountBalances[7][1])/masterAccountBalances[7][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='augNetWorthGrowth'>" + parseFloat((masterAccountBalances[9][1]-masterAccountBalances[8][1])/masterAccountBalances[8][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='sepNetWorthGrowth'>" + parseFloat((masterAccountBalances[10][1]-masterAccountBalances[9][1])/masterAccountBalances[9][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='octNetWorthGrowth'>" + parseFloat((masterAccountBalances[11][1]-masterAccountBalances[10][1])/masterAccountBalances[10][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='novNetWorthGrowth'>" + parseFloat((masterAccountBalances[12][1]-masterAccountBalances[11][1])/masterAccountBalances[11][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='decNetWorthGrowth'>" + parseFloat((masterAccountBalances[13][1]-masterAccountBalances[12][1])/masterAccountBalances[12][1]*100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td></tr>";
+function updateNetWorthGrowthRowDollar() {
+    var monthlyNetWorthGrowths = [];
+    for (var i = 2; i < 14; i++) {
+        var temp = (masterAccountBalances[i][1]-masterAccountBalances[i-1][1]);
+        if (!isNaN(temp) && isFinite(temp)) {
+            monthlyNetWorthGrowths.push(temp);
+        }
+        else {
+            monthlyNetWorthGrowths.push(0);
+        }
+    }
+    return "<tr><td>Growth ($)</td><td id='startNetWorthGrowthDollar'></td><td id='janNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[0]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='febNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='marNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[2]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='aprNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[3]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='mayNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[4]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='junNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[5]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='julNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[6]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='augNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[7]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='sepNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[8]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='octNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[9]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='novNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[10]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td><td id='decNetWorthGrowthDollar'>$" + parseFloat(monthlyNetWorthGrowths[11]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "</td></tr>";
+}
+
+function updateNetWorthGrowthRowPercent() {
+    var monthlyNetWorthGrowths = [];
+    for (var i = 2; i < 14; i++) {
+        var temp = (masterAccountBalances[i][1]-masterAccountBalances[i-1][1])/masterAccountBalances[i-1][1]*100;
+        if (!isNaN(temp) && isFinite(temp)) {
+            monthlyNetWorthGrowths.push(temp);
+        }
+        else {
+            monthlyNetWorthGrowths.push(0);
+        }
+    }
+    return "<tr><td>Growth (%)</td><td id='startNetWorthGrowthPercent'></td><td id='janNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[0]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='febNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[1]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='marNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[2]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='aprNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[3]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='mayNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[4]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='junNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[5]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='julNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[6]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='augNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[7]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='sepNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[8]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='octNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[9]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='novNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[10]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td><td id='decNetWorthGrowthPercent'>" + parseFloat(monthlyNetWorthGrowths[11]).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%</td></tr>";
+}
+
+function updateMasterNetWorthSection() {
+    var totalGrowthDollars = parseFloat(masterAccountBalances[13][1]-masterAccountBalances[1][1]);
+    var totalGrowthPercent = parseFloat((masterAccountBalances[13][1]-masterAccountBalances[1][1])/masterAccountBalances[1][1]*100);
+    var averageMonthlyGrowthDollar = parseFloat((masterAccountBalances[13][1]-masterAccountBalances[1][1])/12);
+    var averageMonthlyGrowthPercent = parseFloat((masterAccountBalances[13][1]-masterAccountBalances[1][1])/masterAccountBalances[1][1]*100/12);
+    document.getElementById("totalGrowthDollars").innerHTML = (!isNaN(totalGrowthDollars) && isFinite(totalGrowthDollars)) ? ("$" + totalGrowthDollars.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')) : ("$0.00");
+    document.getElementById("totalGrowthPercent").innerHTML = (!isNaN(totalGrowthPercent) && isFinite(totalGrowthPercent)) ? (totalGrowthPercent.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%") : ("0.00%");
+    document.getElementById("averageMonthlyGrowthDollar").innerHTML = (!isNaN(averageMonthlyGrowthDollar) && isFinite(averageMonthlyGrowthDollar)) ? ("$" + averageMonthlyGrowthDollar.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')) : ("$0.00");
+    document.getElementById("averageMonthlyGrowthPercent").innerHTML = (!isNaN(averageMonthlyGrowthPercent) && isFinite(averageMonthlyGrowthPercent)) ? (averageMonthlyGrowthPercent.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "%") : ("0.00%");
 }
 
 function updateCell(row,column,id) {
